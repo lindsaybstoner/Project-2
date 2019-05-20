756e62b303217f68695b647c936d33f5b17a2956
fcarlone <frankcarlone@gmail.com>
2019-05-19 21:29:10 -0400

    Config Passport to authenticate users

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,10 @@
 var db = require("../models");
+var isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
@@ -12,16 +13,70 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (dbExample) {
       res.render("example", {
         example: dbExample
       });
     });
   });
 
+  // Dog owner landing page
+  app.get("/dog-owner", function (req, res) {
+    res.render("dog-owner", {
+      msg: "Welcome"
+    });
+  });
+
+  // Dog owner login page
+  app.get("/dog-owner-login", function (req, res) {
+    res.render("dog-owner-login", {
+      msg: "Login"
+    });
+  });
+
+  // Dog Owner Login
+  app.get("/dog-owner-login", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/dog-owner");
+    }
+    res.sendFile(path.join(__dirname, "../public/dog-owner-signup.html"));
+  });
+
+  app.get("/dog-owner-login", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/dog-owner");
+    }
+    res.sendFile(path.join(__dirname, "../public/dog-owner-login.html"));
+  });
+
+  // Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/dog-owner", isAuthenticated, function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/dog-owner.html"));
+  });
+
+  // Dog owner login page
+  app.get("/dog-owner-signup", function (req, res) {
+    res.render("dog-owner-signup", {
+      msg: "Signup"
+    });
+  });
+
+  // Dog Owner Signup
+  // app.get("/dog-owner-signup", function (req, res) {
+  //   // If the user already has an account send them to the members page
+  //   if (req.user) {
+  //     res.redirect("/dog-owner-signup");
+  //   }
+  //   res.sendFile(path.join(__dirname, "../public/dog-owner-signup.html"));
+  // });
+
+
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
 };
--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
+// Passport Packages
+var session = require("express-session");
+var passport = require("./config/passport");
+
+
 var db = require("./models");
 
 var app = express();
@@ -11,6 +16,10 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+// Passport Authentication Middleware
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Handlebars
 app.engine(
@@ -34,8 +43,8 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync(syncOptions).then(function () {
+  app.listen(PORT, function () {
     console.log(
       "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
       PORT,
