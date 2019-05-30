5c521953ccad72ceabd6db6b0cc223631a677dbb
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-05-30 19:26:21 -0400

    get rid of extra code

--- public/js/dog-form.js
+++ public/js/dog-form.js
@@ -56,22 +56,6 @@ $(document).ready(function() {
 
   }
 
-  // Gets dog data for the current dog if we're editing
-  function getDogData(id, type) {
-    $.get(queryUrl, function(data) {
-      if (data) {
-        console.log(data.AuthorId || data.id);
-        // If this post exists, prefill our cms forms with its data
-        titleInput.val(data.title);
-        bodyInput.val(data.body);
-        authorId = data.AuthorId || data.id;
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
-        updating = true;
-      }
-    });
-  }
-
   /* $("#dogFormSubmit").on("click", function(event) {
     console.log("on submit button");
     event.preventDefault();
