669ed6d2b7114bc37e97085e772010cfff45704f
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-05-30 15:01:01 -0400

    get delete button to work

--- public/js/dog-form.js
+++ public/js/dog-form.js
@@ -1,7 +1,40 @@
-/* console.log("HELLO?");
+console.log("HELLO?");
 
-$(document).ready(function() {
-  $("#dogFormSubmit").on("click", function(event) {
+$(document).ready(function () {
+  // Click events for the edit and delete buttons
+  $(document).on("click", "button.delete", handleDogDelete);
+  $(document).on("click", "button.edit", handlePostEdit);
+
+  // This function does an API call to delete dog
+  function deleteDog(id) {
+    $.ajax({
+      method: "DELETE",
+      url: "/api/dog_data/" + id
+    }).then(function () {
+      console.log("dog has been deleted");
+      window.location.reload();
+    });
+  }
+
+  function handleDogDelete() {
+    var id = $(this).data("id");
+    console.log(id);
+    /* console.log(dogId); */
+    deleteDog(id);
+  }
+
+  /* // This function does an API call to edit the dogs info
+  function editDog(id) {
+    $.ajax({
+      method: "DELETE",
+      url: "/api/dog_data/" + id
+    }).then(function () {
+      console.log("dog has been deleted");
+      window.location.reload();
+    });
+  } */
+
+  /* $("#dogFormSubmit").on("click", function(event) {
     console.log("on submit button");
     event.preventDefault();
     var dogName = $("#dog-name");
@@ -53,6 +86,5 @@ $(document).ready(function() {
       // add userId?
     });
   }
-  // function to get and display dogs once form is submitted
+  // function to get and display dogs once form is submitted */
 });
- */
