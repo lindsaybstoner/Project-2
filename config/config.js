5bdb66b9d4e64c277b660155e08f566e23fbdb01
Blank <ellenmacy@Lindsays-MacBook-Pro.local>
2019-05-30 19:35:22 -0400

    fix config for production

--- config/config.js
+++ config/config.js
@@ -15,10 +15,8 @@ module.exports = {
   },
   production: {
     // eslint-disable-next-line camelcase
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "vrw0ndn37554r84u",
-    host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
