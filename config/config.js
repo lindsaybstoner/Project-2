e511fa4b174f94ccbe93d4c35606c001af569159
DocHolidayDeluxe <dabauer@me.com>
2019-05-30 21:36:36 -0400

    new date

--- config/config.js
+++ config/config.js
@@ -5,10 +5,6 @@ module.exports = {
     database: "dogapp_db",
     host: "127.0.0.1",
     port: 3306,
-<<<<<<< HEAD
-=======
-
->>>>>>> 4cee43a3ad5c5eca5690d1c1cdcae385a38c44ce
     dialect: "mysql"
   },
   test: {
@@ -21,16 +17,8 @@ module.exports = {
   },
   production: {
     // eslint-disable-next-line camelcase
-<<<<<<< HEAD
     // eslint-disable-next-line camelcase
     use_env_variable: "JAWSDB_URL",
-=======
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: "dogapp_db",
-    host: "127.0.0.1",
-    port: 3306,
->>>>>>> 4cee43a3ad5c5eca5690d1c1cdcae385a38c44ce
     dialect: "mysql"
   }
 };
